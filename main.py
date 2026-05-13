3985d07df28e646d26cffffc512e8c08345b0cbc
AvtNic <28092021ukr@gmail.com>
2026-05-13 21:34:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello, Git")
+
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/AvtNic/homework_git.git
+# git push -u origin main
